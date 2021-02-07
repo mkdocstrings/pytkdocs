8b1918d19735be24e0f62fd672c61d6797d4cd7c
Timothée Mazzucotelli <pawamoy@pm.me>
2021-02-07 13:10:23 +0100

    wip

---
+++ src/pytkdocs/t1.py
@@ -0,0 +1,5 @@
+from pytkdocs.t2 import s, t, E, Z
+
+class D:
+    v = E.A
+    w = Z.A
---
+++ src/pytkdocs/t2.py
@@ -0,0 +1,15 @@
+import enum
+
+class S:
+    pass
+
+
+s = S()
+t = "t"
+
+class E(enum.Enum):
+    A = 0
+
+
+class Z:
+    A = 0
